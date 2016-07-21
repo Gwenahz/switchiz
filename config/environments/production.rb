809f756f83aa7f6d224d32f40f5987521aca632b
Gwenael Ahoomey <ahoomey@gmail.com>
2016-07-21 15:19:12 +0200

    AWS S3 config region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
+    bucket: ENV.fetch('switchiz-bucket'),
+    access_key_id: ENV.fetch('AKIAIU4H7FMZ2COB74UQ'),
+    secret_access_key: ENV.fetch('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'),
+    s3_region: ENV.fetch('eu-central-1'),
   }
 }
 end
